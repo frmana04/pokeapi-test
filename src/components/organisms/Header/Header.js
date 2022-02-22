72fe5eee9f2a91e3481ff18fbc1d71aa00608517
frmana04 <fjmn2c@gmail.com>
2022-02-22 14:28:19 +0100

    component header

--- src/components/organisms/Header/Header.js
+++ src/components/organisms/Header/Header.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./Header.scss";
-
+import ImageHeader from 'assets/images/cabecera.jpg'
 function Header() {
   return (
     <>
-      <h1>Header</h1>
+      
+      <div className="header">
+          <img className="header__img" src={ImageHeader} alt="header"/>
+          <h3 className="header__title">PokeApi Test</h3>
+      </div>
 
-  
     </>
   );
 }
