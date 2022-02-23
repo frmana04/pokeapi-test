32c0d2437041726a20c1f8b27fed282183e6e14e
frmana04 <fjmn2c@gmail.com>
2022-02-23 12:53:43 +0100

    moves details list

--- src/components/pages/DetailPokemon/DetailPokemon.js
+++ src/components/pages/DetailPokemon/DetailPokemon.js
@@ -3,15 +3,29 @@ import { useLocation } from "react-router-dom";
 import LinkTo from "components/atoms/LinkTo/LinkTo";
 import "./DetailPokemon.scss";
 import { getPokemonForms } from "services/getPokemonForms";
+import { Chip } from "@mui/material";
+import { setId } from "helpers/helpers";
+
 function DetailPokemon() {
   const { state } = useLocation();
+  state.moves = setId(state.moves);
+
   const [dataForm,updateIsDataForm] = useState({id:'',is_battle_only:''})
+  const [movesList,updateMovesList] = useState(state.moves)
+
   const getForms = async() =>{
       const {id,is_battle_only} = await getPokemonForms(state.forms[0].url);
      
       updateIsDataForm({id,is_battle_only});
   }
 
+  const handleDeleteMove = (id)=>{
+    console.log("jeje:",id);
+    if (id){
+      const newList = movesList.filter(oneMove=>oneMove.id!==id)
+      updateMovesList(newList);
+    }
+  }
   useEffect(()=>{
     window.scrollTo(0, 0);
     getForms();
@@ -19,8 +33,7 @@ function DetailPokemon() {
 
   return (
     <>
-   
-   
+      
       <LinkTo url={"home"} text={"Home"} />
       <LinkTo url={"list-pokemon"} text={"Pokemon List"} />
       <h2 className="detail__title">Details</h2>
@@ -47,15 +60,16 @@ function DetailPokemon() {
    
       </div>
 
-      <div>
-          <p className="detail__move-title">Moves</p>
-
-          {state.moves.map((move) => (
-            <p>{move.move.name}</p>
-          ))}
-        </div>
+       <div className="detail__moves-block">
+          <p className="detail__moves-title">Moves</p>
+        <div className="detail__moves-list">
+          {movesList.map((move) => 
+              <Chip className="detail__moves-move" label={move.move.name} variant="outlined" onDelete={()=>handleDeleteMove(move.id)}  />
+          )}
+        </div> 
+        </div>  
     </>
   );
-}
+      }
 
 export default DetailPokemon;
